d242f3de477c4fec98adef994e787e28d2065213
low-perry <dariohaxhiraj@hotmail.com>
2025-03-04 19:02:06 +0100

    Add contact information, annd text to hero, handle margins of the text when nav dropdown is active

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,11 +1,14 @@
 document.addEventListener("DOMContentLoaded", function() {
     const toggleButton = document.getElementById("navbar-toggle");
     const navbarLinks = document.getElementById("navbar-links");
+    const heroContainer = document.querySelector(".hero-container");
 
     // @ts-ignore
     toggleButton.addEventListener("click", function() {
         // @ts-ignore
         navbarLinks.classList.toggle("active");
+        // @ts-ignore
+        heroContainer.classList.toggle("dropdown-active");
     });
 
     // Close the dropdown when clicking outside of it
@@ -14,6 +17,8 @@ document.addEventListener("DOMContentLoaded", function() {
         if (!navbarLinks.contains(event.target) && !toggleButton.contains(event.target)) {
             // @ts-ignore
             navbarLinks.classList.remove("active");
+            // @ts-ignore
+            heroContainer.classList.remove("dropdown-active");
         }
     });
 
@@ -22,6 +27,8 @@ document.addEventListener("DOMContentLoaded", function() {
         if (event.key === "Escape") {
             // @ts-ignore
             navbarLinks.classList.remove("active");
+            // @ts-ignore
+            heroContainer.classList.remove("dropdown-active");
         }
     });
 
--- blog.markdown
+++ blog.markdown
@@ -4,4 +4,3 @@ title: Blog
 permalink: /blog/
 ---
 
-This is the blog
