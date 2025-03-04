3d56d97ab999fecaa522039c5335a700f68ff08d
low-perry <dariohaxhiraj@hotmail.com>
2025-03-04 17:24:34 +0100

    Fix navbar bugs

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,4 +5,21 @@ document.addEventListener("DOMContentLoaded", function() {
     toggleButton.addEventListener("click", function() {
         navbarLinks.classList.toggle("active");
     });
+
+    // Close the dropdown when clicking outside of it
+    document.addEventListener("click", function(event) {
+        if (!navbarLinks.contains(event.target) && !toggleButton.contains(event.target)) {
+            navbarLinks.classList.remove("active");
+        }
+    });
+
+     // Close the dropdown when pressing the Esc key
+     document.addEventListener("keydown", function(event) {
+        if (event.key === "Escape") {
+            navbarLinks.classList.remove("active");
+        }
+    });
+
+
 });
+
--- index.markdown
+++ index.markdown
@@ -1,8 +1,6 @@
 ---
-# Feel free to add content and custom Front Matter to this file.
-# To modify the layout, see https://jekyllrb.com/docs/themes/#overriding-theme-defaults
-
 layout: main
 ---
-
-# This is real this is me.
+<p style="background: linear-gradient(to right, #f5e0dc, #cba6f7); -webkit-background-clip: text; -webkit-text-fill-color: transparent; font-size: 72px; font-weight: bold;">
+  This is some gradient text.
+</p>
